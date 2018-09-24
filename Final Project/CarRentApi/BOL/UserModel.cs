df5c34a8f497947fa6171b215073fab9d9e3bb7b
Marina <35783670+Mar1Mau@users.noreply.github.com>
2018-09-25 00:52:26 +0300

    Update UserModel.cs

--- Final Project/CarRentApi/BOL/UserModel.cs
+++ Final Project/CarRentApi/BOL/UserModel.cs
@@ -9,7 +9,7 @@ namespace BOL
         public string FullName { get; set; }
 
         [Required]
-        //[IdValidator]
+        [IdValidator]
         public string IdCard { get; set; }
 
         [Required, MinLength(5), MaxLength(20)]
